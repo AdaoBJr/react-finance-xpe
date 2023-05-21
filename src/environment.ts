a8689729cc817ce01bff275296fcc3ae7ef39850
AdaoBJr <adaobjr@gmail.com>
2023-05-20 22:08:36 -0400

    fix: :green_heart: updated environment url

--- src/environment.ts
+++ src/environment.ts
@@ -7,6 +7,6 @@ export type Environment = 'dev' | 'prd' | 'test';
 export const getEnv = () => ({
   ENV: [dev, prd, test].find(Boolean)! as Environment,
   URL_BASE: prd
-    ? 'https://finance-db-xpe.vercel.app/data'
+    ? 'https://finance-db-xpe.vercel.app/despesas'
     : 'http://localhost:4000/despesas',
 });
